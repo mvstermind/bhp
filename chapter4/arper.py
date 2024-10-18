69aff1b3e7b235d305d6c1cb3ae9ab633ef21c1c
mvstermind <zablockidawid1@gmail.com>
2024-10-18 19:10:24 +0200

    class initialized

--- chapter4/arper.py
+++ chapter4/arper.py
@@ -7,12 +7,27 @@ import time
 
 
 def get_mac(target_ip):
-    pass
+    packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op="who-has", pdst=targetip)
+    resp, _ = srp(packet, timeout=2, retry=10, verbose=False)
+    for _, r in resp:
+        return r[Ether].src
+    return None
 
 
 class Arper:
     def __init__(self, victim, gateway, interface="en0"):
-        pass
+        self.victim = victim
+        self.victimmac = get_mac(victim)
+        self.gateway = gateway
+        self.gatewaymac = get_mac(gateway)
+        self.interface = interface
+        conf.iface = interface
+        conf.verb = 0
+
+        print(f"Initialized {interface}:")
+        print(f"Gateway ({gateway}) is at {self.gatewaymac}.")
+        print(f"Victim ({victim}) is at {self.victimmac}.")
+        print("-" * 30)
 
     def run(self):
         pass
@@ -29,5 +44,3 @@ class Arper:
 
 if __name__ == "__main__":
     (victim, gateway, interface) = (sys.argv[1], sys.argv[2], sys.argv[3])
-    myarp = Arper(victim, gateway, interface)
-    myarp.run()
