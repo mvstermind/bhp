a01d6cd20dd5b6df731f72823d534c5a62b1682c
mvstermind <zablockidawid1@gmail.com>
2024-10-18 19:11:26 +0200

    we runnin boys

--- chapter4/arper.py
+++ chapter4/arper.py
@@ -30,7 +30,11 @@ class Arper:
         print("-" * 30)
 
     def run(self):
-        pass
+        self.poison_thread = Process(target=self.poison)
+        self.poison_thread.start()
+
+        self.sniff_thread = Process(target=self.sniff)
+        self.sniff_thread.start()
 
     def poison(self):
         pass
