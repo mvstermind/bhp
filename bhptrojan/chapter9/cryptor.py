f6b5e384c4e18d5bafacb97144f8a4245bd85533
mvstermind <zablockidawid1@gmail.com>
2024-10-23 07:39:58 +0200

    WE CRYPTOGRAPHY

---
+++ bhptrojan/chapter9/cryptor.py
@@ -0,0 +1,19 @@
+from Cryptodome.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from io import BytesIO
+
+import base64
+import zlib
+
+
+def generate():
+    new_key = RSA.generate(2048)
+    private_key = new_key.exportKey()
+    public_key = new_key.publickey().exportKey()
+
+    with open("key.pri", "wb") as f:
+        f.write(private_key)
+
+    with open("key.pub", "wb") as f:
+        f.write(public_key)
